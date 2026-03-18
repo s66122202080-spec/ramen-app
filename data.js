8142740165d0d49bc27f9cb39f5050043f6a6e7c
s66122202080-spec <s66122202080@ssru.ac.th>
2026-03-18 17:58:37 +0700

    Update data.js

--- data.js
+++ data.js
@@ -57,7 +57,7 @@ const RAMEN_MENU = [
     tags: ['เผ็ดสุด!', 'ร้อนแรง', 'ท้าทาย'],
     color: '#E67E22',
     emoji: '🌶️',
-    image: 'https://raw.githubusercontent.com/s66122202080-spec/ramen-app/refs/heads/main/yon.jpg',
+    image: 'https://s66122202080-spec.github.io/ramen-app/yon.jpg',
     imageFallback: 'https://s66122202080-spec.github.io/ramen-app/yeol.jpg'
   },
   {
